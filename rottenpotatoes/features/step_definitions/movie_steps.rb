ccd14edacd0e8bbac21e0e9dbefa185e327effd4
Brian Gibson <bggibson@hotmail.com>
2019-05-05 13:00:19 -0400

    Part 2, #1 and #2

--- rottenpotatoes/features/step_definitions/movie_steps.rb
+++ rottenpotatoes/features/step_definitions/movie_steps.rb
@@ -8,7 +8,7 @@ Given /the following movies exist/ do |movies_table|
   end
 end
 
-Then /(.*) seed movies should exist/ do | n_seeds |
+Then /(.*) seed movies should exist/ do |n_seeds|
   Movie.count.should be n_seeds.to_i
 end
 
@@ -26,10 +26,13 @@ end
 #  "When I check the following ratings: G"
 
 When /I (un)?check the following ratings: (.*)/ do |uncheck, rating_list|
-  # HINT: use String#split to split up the rating_list, then
-  #   iterate over the ratings and reuse the "When I check..." or
-  #   "When I uncheck..." steps in lines 89-95 of web_steps.rb
-  fail "Unimplemented"
+  rating_list.split(",").each do |rating|
+    if uncheck === "un"
+      steps %Q{And I uncheck "#{rating.strip}"}
+    else
+      steps %Q{And I check "#{rating.strip}"}
+    end
+  end
 end
 
 Then /I should see all the movies/ do
